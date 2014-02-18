c552b3d905d19c751659472b35e7b9416aec5f70
elliospizzaman <elliospizzaman@users.noreply.github.com>
2014-02-18 16:08:45 -0500

    Create cards.py

---
+++ cards.py
@@ -0,0 +1,78 @@
+import random
+ 
+BOTTOM, RANDOM = 'bottom', 'random'
+ 
+class Card(object):
+	"""docstring for Card"""
+	def __init__(self, _rank, _suit, _rankname, _suitname):
+		self._rank = _rank
+		self._suit = _suit
+		self._rankname = _rankname
+		self._suitname = _suitname
+		
+	def getSuit(self):
+		return self._suit
+		
+	def getRank(self):
+		return self._rank
+		
+	def setSuit(self, suit):
+		self._suit = suit
+		
+	def setRank(self, rank):
+		self._rank = rank
+		
+	def __str__(self):
+		return "%s of %s" % (self._rankname, self._suitname)
+ 
+	def __cmp__(self,other):
+		if self._rank < other._rank:
+			return -1
+		if self._rank > other._rank:
+			return 1
+		return 0		
+ 
+class Deck(list):
+	"""docstring for Deck"""
+	def __init__(self, ranks=None, suits=None):
+		self.ranks = ranks or [
+		"Duece", "Three", "Four", "Five",
+		"Six", "Seven", "Eight", "Nine", "Ten",
+		"Jack", "Queen", "King", "Ace"]
+		self.suits = suits or [
+		"Clubs", "Diamonds", "Hearts", "Spades"
+		]
+		for rank, rankname in enumerate(self.ranks):
+			for suit, suitname in enumerate(self.suits):
+				self.append(Card(rank,suit,rankname,suitname))
+ 
+ 
+ 
+	def draw(self, which=None):
+		"draw the top card ( removes that card )"
+		try:
+			which = which.lower()
+			if which == 'bottom':
+				return self.pop(0)
+			if which == 'random':
+				return self.pop(random.randint(0,len(self)-1))
+			return self.pop()
+		except:
+			return self.pop()
+			
+ 
+	def shuffle(self):
+		"shuffle the deck"
+		pass
+ 
+	def cut(self):
+		"split the deck in two and put the top half ( about half ) on the bottom"
+		pass
+ 
+	def add(self,card):
+		"put a card on top of the deck"
+		pass
+ 
+	def push(self, card):
+		"put a card on the bottom of the deck"
+		pass
